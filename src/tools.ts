b2d701f30cd78793d574126c91589d6f32d24846
mickceb <cebulski.mi@gmail.com>
2024-04-15 14:37:15 +0200

    init functions

---
+++ src/tools.ts
@@ -0,0 +1,43 @@
+import type { Location } from "./types"
+
+export async function getCurrentWeather({ location }) {
+  const weather = {
+      location,
+      temperature: "75",
+      forecast: "sunny"
+  }
+  return JSON.stringify(weather)
+}
+
+export async function getLocation() {
+  try {
+      const response = await fetch('https://ipapi.co/json/')
+      const text = await response.json()
+      return JSON.stringify(text)
+  } catch (err) {
+      console.log(err)
+  }
+}
+
+export const functions = [
+  {
+      function: getCurrentWeather,
+      parameters: {
+          type: "object",
+          properties: {
+              location: {
+                  type: "string",
+                  description: "The location from where to get the weather"
+              }
+          },
+          required: ["location"]
+      }
+  },
+  {
+      function: getLocation,
+      parameters: {
+          type: "object",
+          properties: {}
+      }
+  },
+]
